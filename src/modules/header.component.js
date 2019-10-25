566062fe7c201ff2a9b5887d0200ebc169525f2b
Elizaveta <vinokurova.e@brinex.ru>
2019-10-25 19:16:47 +0300

    commit

---
+++ src/modules/header.component.js
@@ -0,0 +1,6 @@
+const header = 'Some header';
+
+const func = () => parseInt(header, 10);
+func();
+console.log(func());
+console.log('Header component');
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        })
+    ]
 
 };
